00d9493a781ba6d3ad55a9207a52781555dbc7ea
Joseph Whitman <jwhitman89@gmail.com>
2015-08-06 18:53:37 -0400

    P9
    First attempt, need to add more pythag triplet conditions to function

---
+++ euler-009.py
@@ -0,0 +1,11 @@
+# Project Euler Problem 9
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a^2 + b^2 = c^2
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+def ispytriple(x):
+	"Check if 3 numbers a,b,c are a Pythagorean triplet"
+	if a+b < c:
+		return False
+	
